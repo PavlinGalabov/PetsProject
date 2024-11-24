55c808307dbf57f9455397cdecd2ec8a196d5359
PavlinGalabov <pavlin.galabov@gmail.com>
2024-11-25 00:36:17 +0200

    create template inheritance

--- PetsProject/common/views.py
+++ PetsProject/common/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
+from PetsProject.pets.models import Pet
+
 
 # Create your views here.
 def index(request):
-    context = {}
+    context = {
+        'pet_photos': Pet.objects.all()
+    }
     return render(request, "common/home-page.html", context)
+
